e3fa2b0dbba4c05be14eecfa45b868edf99890f2
lemonilemon <87176609+lemonilemon@users.noreply.github.com>
2021-12-23 12:15:45 +0800

    Second commit

--- js/jquery.jSlots.js
+++ js/jquery.jSlots.js
@@ -13,6 +13,23 @@ function shuffle(array) {
         [array[i], array[j]] = [array[j], array[i]];
     }
 }
+var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = "http://code.jquery.com/jquery-2.2.1.min.js";
+
+// Then bind the event to the callback function.
+// There are several events for cross browser compatibility.
+//script.onreadystatechange = handler;
+script.onload = handler;
+
+// Fire the loading
+head.appendChild(script);
+
+function handler(){
+    console.log('jquery added :)');
+}
+
 
 (function($){
 
